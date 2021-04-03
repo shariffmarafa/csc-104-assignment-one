bca8ed20deebe055e17a294501333bfaedf106e3
shariffmarafa <81803460+shariffmarafa@users.noreply.github.com>
2021-04-03 12:56:50 +0100

    Add files via upload

---
+++ number-1.py
@@ -0,0 +1,30 @@
+print('Question ONE')
+NAME='SHARIFF SULEIMAN MARAFA'
+DEPT='COMPUTER SCIENCE DEPARTMENT'
+print('my name is',NAME,'i am from',DEPT)
+print('Question TWO')
+scare='Boo!'
+
+print(scare)
+print('Question THREE')
+Number1=6
+Number2=3
+Number3=Number1 + Number2
+print(Number1,Number2,Number3)
+print('Question FOUR')
+Calculation=12/0.5
+print(Calculation)
+print('Question FIVE')
+Addition=5.1 + 6
+print(Addition)
+print('Question SIX')
+marafa=8%3
+print(marafa)
+print('Question SEVEN')
+ResultA=2 * 4
+
+ResultB=4 + 2
+
+ResultC=ResultA * ResultB
+
+print(ResultC)
