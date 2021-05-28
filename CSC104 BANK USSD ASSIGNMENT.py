cfe73045d00b9df01e07690cb489e1c33fe75e43
Shariff Marafa <81803460+shariffmarafa@users.noreply.github.com>
2021-05-28 23:53:15 +0100

    Add files via upload

---
+++ CSC104 BANK USSD ASSIGNMENT.py
@@ -0,0 +1,153 @@
+print('NAME: SHARIFF SULEIMAN MARAFA FROM COMPUTER SCIENCE DEPT.\n MATRIC NUMBER:U19/FNS/CSC/1104')
+print("WELCOME TO UBA USSD TRANSACTION")
+# 	ussd_code = int(input("Enter ussd code here: "))
+def back():
+	if len(pin) == 4:
+		print('your Account Balance is',balance,'Naira')
+while True:
+	Account_name = ['SHARIFF MARAFA']
+	Account_number = []
+	Account_pin = 1414
+	balance = 7500000
+	ussd_code = input("Enter ussd code here: ")
+	if ussd_code == '*919#':
+		print('UBA')
+		print("1.Airtime self\n2.Airtime others\n3.Transfer UBA\n4.Transfer other banks\n5. Transfer UBA prepaid\n6.check balance\n7.Pay bills\n8.Next.")
+		option = int(input('select option:'))
+		if option == 1:
+			pin = input('Enter your pin')
+			if len(pin) == 4:
+				amount = int (input('Please enter amount:'))
+				print('Transaction succeful')
+				break
+			else:
+				print('Wrong pin')
+				pin = input('Enter pin to continue:')
+				back()
+			break
+		elif option == 2:
+			pin = input('Enter your pin')
+			if len(pin) ==4:
+			   	amount = int(input('Enter amount:'))
+			   	int (input('please enter destination phone number:'))
+			   	print('Transaction succeful')
+			   	break
+			else:
+				print('Wrong pin')
+				pin = input('Enter pin to continue:')
+				back()
+			break
+		elif option==3:
+			pin = input('Enter your pin')
+			if len(pin) ==4:
+				amount = int(input('Enter amount:'))
+				int (input('please enter UBA account number:'))
+				print('Transaction succeful')
+				break
+			else:
+				print('Wrong pin')
+				pin = input('Enter pin to continue:')
+				back()
+			break
+		elif option==4:
+			pin = input('Enter your pin')
+			if len(pin) ==4:
+				int (input('please enter account number of the beneficiary'))
+				amount = int(input('Enter amount:'))
+				print('select bank')
+				print("1.Access bank\n2.City bank nig limited\n3.Eco bank Nigeria plc\n4.FidelityBank plc\n5.First bank Nigeria limited\n6.First city monument bankplc\n7.GTB plc\n8.Unity bank \n9. Wema bank \n10.Zenith.")
+				option = int(input('select option:'))
+				if option == 1:
+					pin = input('Enter your pin')
+					if len(pin) == 4:
+						print('Transaction succeful')
+				elif option == 2:
+					pin = input('Enter your pin')
+					if len(pin) == 4:
+						print('Transaction succeful')
+				elif option == 3:
+					pin = input('Enter your pin')
+					if len(pin) == 4:
+						print('Transaction succeful')
+				elif option == 4:
+					pin = input('Enter your pin')
+					if len(pin) == 4:
+						print('Transaction succeful')
+				elif option == 5:
+					pin = input('Enter your pin')
+					if len(pin) == 4:
+						print('Transaction succeful')
+				elif option == 6:
+					pin = input('Enter your pin')
+					if len(pin) == 4:
+						print('Transaction succeful')
+				elif option == 7:
+					pin = input('Enter your pin')
+					if len(pin) == 4:
+						print('Transaction succeful')
+				elif option == 8:
+					pin = input('Enter your pin')
+					if len(pin) == 4:
+						print('Transaction succeful')
+				elif option == 9:
+					pin = input('Enter your pin')
+					if len(pin) == 4:
+						print('Transaction succeful')
+				elif option == 10:
+					pin = input('Enter your pin')
+					if len(pin) == 4:
+						print('Transaction succeful')
+					break
+			else:
+				print('Wrong pin')
+				pin = input('Enter pin to continue:')
+				back()
+			break
+		elif option==5:
+			pin = input('Enter your pin')
+			if len(pin) ==4:
+				int (input('please enter card client ID (full 10 digit) or name of the beneficiary'))
+				print('Transaction succeful')
+			break
+		elif option == 6:
+			pin = input('Enter your pin')
+			if len(pin) ==4:
+				print('your account bal: 5000000')
+			break
+		elif option== 7:
+			pin = input('Enter your pin')
+			if len(pin) ==4:
+				amount = int(input('Enter amount:'))
+				print('please select')
+				print("1.cable TV\n2.Betting l lottery\n3.Electricity - prepaid\n4. Electricity Post paid\n5.Tolls \n6.Next.")
+				option = int(input('select option:'))
+				if option == 1:
+					pin = input('Enter your pin')
+					if len(pin) == 4:
+						print('Transaction succeful')
+				elif option == 2:
+					pin = input('Enter your pin')
+					if len(pin) == 4:
+						print('Transaction succeful')
+				elif option == 3:
+					pin = input('Enter your pin')
+					if len(pin) == 4:
+						print('Transaction succeful')
+				elif option == 4:
+					pin = input('Enter your pin')
+					if len(pin) == 4:
+						print('Transaction succeful')
+				elif option == 5:
+					pin = input('Enter your pin')
+					if len(pin) == 4:
+						print('Transaction succeful')
+				elif option == 6:
+					pin = input('Enter your pin')
+					if len(pin) == 4:
+						print('Transaction succeful')
+				break
+			else:
+				print('Wrong pin')
+				pin = input('Enter pin to continue:')
+				back()
+			break
